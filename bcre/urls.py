1ff0a49f23ec88f8c797d476065e3c989c1a6ad8
wing <wingkwmak2@yahoo.com.hk>
2024-12-23 17:37:51 +0800

    step 6

--- bcre/urls.py
+++ bcre/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('', include('pages.urls')),    #pages= app
+    path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),    #admin.site= virtual environmt    
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + debug_toolbar_urls()
